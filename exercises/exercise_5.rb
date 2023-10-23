833468a9924afeb6020f25cf79a8ee544d4a7bfa
Bo Yang Yu <boyangyu93@gmail.com>
2023-10-22 22:12:08 -0400

    print total revenue, average revenue, and store with revenue greater than 1mil

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+totalRevenue = Store.sum(:annual_revenue);
+puts totalRevenue;
+puts totalRevenue / Store.count;
+puts Store.where("annual_revenue > 1000000").count;
