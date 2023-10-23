a7672b2df24bf4a8066955e2bb28e85423fd15fe
Bo Yang Yu <boyangyu93@gmail.com>
2023-10-22 22:21:02 -0400

    create link between employees and store, create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Samma", last_name: "Su", hourly_rate: 100)
+@store1.employees.create(first_name: "Edward", last_name: "Whitebeard", hourly_rate: 1000)
+@store2.employees.create(first_name: "Jonathan", last_name: "Joestar", hourly_rate: 85)
+@store2.employees.create(first_name: "Jo", last_name: "Jo", hourly_rate: 12)
+@store2.employees.create(first_name: "Bo Yang", last_name: "Yu", hourly_rate: 8)
