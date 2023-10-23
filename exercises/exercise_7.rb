17fbf616b5b24a9e372f75f44c8afb9813056cb6
Bo Yang Yu <boyangyu93@gmail.com>
2023-10-22 22:45:47 -0400

    add validations for store and employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,24 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name"
+
+store_name_input = gets.chomp
+
+new_store = Store.create(name: store_name_input)
+
+puts new_store.errors.full_messages
+
+# puts "Please enter how much revenue per year"
+
+# store_revenue_input = gets.chomp
+
+# puts "You sale men's cloth?"
+
+# store_men_input = gets.chomp == "Yes" ? true : false
+
+# puts "How about women?"
+
+# store_women_input = gets.chomp == "Yes" ? true : false
+
+# Store.create(name: store_name_input, annual_revenue: store_revenue_input, mens_apparel: store_men_input, womens_apparel: store_women_input)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_inclusion_of :hourly_rate, in: 40..200
   belongs_to :store
+  validates_associated :store
 end
